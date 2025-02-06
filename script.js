b31ddad763c7fa3882a24f79457c52da171a7f03
Edwin Lam <150505703+FrogwinX@users.noreply.github.com>
2025-02-06 20:56:59 +0800

    Change localhost

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function executeQuery() {
     const id = document.getElementById('idInput').value;
 
     try {
-        const response = await fetch(`http://localhost:8080/query?id=${id}`);
+        const response = await fetch(`https://edwintest-backend.azurewebsites.net/query?id=${id}`);
         const data = await response.text();
 
         document.getElementById('displayName').innerText = `Name: ${data}`;
